6108d08a5e4760f74604680a6bdb7cf54c6f625c
Matias Chomicki <matias.chomicki@vividcortex.com>
2018-03-14 15:33:51 -0300

    Use typescript example as starting point

---
+++ src/datasource.ts
@@ -0,0 +1,36 @@
+///<reference path="../node_modules/grafana-sdk-mocks/app/headers/common.d.ts" />
+
+import _ from 'lodash';
+
+export default class VividCortexMetricsDatasource {
+  id: number;
+  name: string;
+
+  /** @ngInject */
+  constructor(instanceSettings, private backendSrv, private templateSrv, private $q) {
+    this.name = instanceSettings.name;
+    this.id = instanceSettings.id;
+
+    console.log(arguments);
+  }
+
+  query(options) {
+    throw new Error("Query Support not implemented yet.");
+  }
+
+  annotationQuery(options) {
+    throw new Error("Annotation Support not implemented yet.");
+  }
+
+  metricFindQuery(query: string) {
+    throw new Error("Template Variable Support not implemented yet.");
+  }
+
+  testDatasource() {
+    return this.$q.when({
+      status: 'error',
+      message: 'Data Source is just a template and has not been implemented yet.',
+      title: 'Error'
+    });
+  }
+}
