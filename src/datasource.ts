4de4adc28c655fcdbc25479afccdb52e6ef8dbce
Matias Chomicki <matias.chomicki@vividcortex.com>
2018-03-20 19:35:59 -0300

    Query series endpoint and receive data from a selected metric

--- src/datasource.ts
+++ src/datasource.ts
@@ -13,7 +13,30 @@ export default class VividCortexMetricsDatasource {
   }
 
   query(options) {
-    return this.$q.when({data: []});
+    const parameters = this.getQueryParameters(options);
+
+    if (! parameters) {
+      return this.$q.when({data: []});
+    }
+
+    const params = {
+      from: parameters.params.from,
+      rank: 1,
+      samplesize: 12,
+      until: parameters.params.from,
+      host: '0,211',
+    };
+
+    const body = {
+      metrics: parameters.metric,
+    };
+
+    return this.doRequest('metrics/query-series', 'POST', params, body)
+      .then(response => {
+        console.warn(response);
+
+        return [];
+      })
   }
 
   annotationQuery(options) {
@@ -23,13 +46,8 @@ export default class VividCortexMetricsDatasource {
   metricFindQuery(query: string) {
     return this.doRequest('metrics', 'GET')
       .then(response => response.data.data || [])
-      .then(metrics => {
-        console.warn(metrics);
-
-        return metrics.map((metric) => {
-          return metric.name;
-        });
-      });
+      .then(metrics => metrics.map(metric => ({ text: metric.name, value: metric.name })))
+      .then(metrics => metrics.filter(metric => metric.text.indexOf(query) > -1));
   }
 
   testDatasource() {
@@ -43,7 +61,7 @@ export default class VividCortexMetricsDatasource {
       title: 'Credentials error'
     };
 
-    return this.doRequest('metrics?limit=1', 'GET')
+    return this.doRequest('metrics', 'GET', { limit: 1 })
       .then(response => {
         if (response.status === 200) {
           return success;
@@ -58,19 +76,48 @@ export default class VividCortexMetricsDatasource {
   /**
    * Perform an HTTP request.
    *
-   * @param  {Object} options
+   * @param  {string} endpoint
+   * @param  {string} method
+   * @param  {Object} params
+   * @param  {Object} body
    * @return {Promise}
    */
-  doRequest(endpoint, method) {
+  doRequest(endpoint, method, params = {}, body = {}) {
     const options = {
       headers: {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer '+this.apiToken,
       },
+      params: params,
       url: 'https://'+this.org+'.app.vividcortex.com/api/v2/'+endpoint,
       method: method,
+      data: body,
     };
 
     return this.backendSrv.datasourceRequest(options);
+  }
+
+
+  /**
+   * Takes a Grafana query object and returns an object with the required information to query
+   * VividCortex, or null if there is an error or no selected metrics.
+   *
+   * @param  {Object} options Grafana options object
+   * @return {Object|null}
+   */
+  getQueryParameters(options) {
+    const metric = options.targets.reduce((metric, target) => {
+      return target.target != 'select metric' ? target.target : metric;
+    }, null);
+
+    if (!metric) { return null; }
+
+    return {
+      metric: metric,
+      params: {
+        from: options.range.from.unix(),
+        until: options.range.to.unix()
+      }
+    };
   }
 }
