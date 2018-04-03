2c10e93b8d0f35fda4f186c7c95a6370d905129b
Matias Chomicki <matias.chomicki@vividcortex.com>
2018-04-03 19:10:57 -0300

    Remove distributed code
    Since we're not releasing yet, there is no point on keeping these files. Yet.

--- dist/module.d.ts
+++
@@ -1,7 +0,0 @@
-import VividCortexMetricsDatasource from './datasource';
-import { VividCortexMetricsQueryCtrl } from './query_ctrl';
-import { VividCortexMetricsConfigCtrl } from './config_ctrl';
-declare class VividCortexMetricsAnnotationsQueryCtrl {
-    static templateUrl: string;
-}
-export { VividCortexMetricsDatasource as Datasource, VividCortexMetricsQueryCtrl as QueryCtrl, VividCortexMetricsConfigCtrl as ConfigCtrl, VividCortexMetricsAnnotationsQueryCtrl as AnnotationsQueryCtrl };
