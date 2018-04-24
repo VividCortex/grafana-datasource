1d83329dcfa1fd675d90daa6afdb36bc81691a5d
Matias Chomicki <matias.chomicki@vividcortex.com>
2018-04-24 16:41:49 -0300

    Add datasource test test

--- specs/datasource.spec.ts
+++ specs/datasource.spec.ts
@@ -1,12 +1,25 @@
 import { expect } from 'chai';
 import 'mocha';
+import $q from 'q';
 import VividCortexDatasource from '../src/datasource';
-import { backendSrv, templateSrv, $q } from './lib/mocks';
+import { backendSrv, templateSrv } from './lib/mocks';
 
 const datasource = new VividCortexDatasource({ jsonData: { apiToken: 1234 } }, backendSrv, templateSrv, $q);
 
 describe('VividCortex datasource', () => {
   it('should be instantiable', () => {
     expect(datasource).not.to.be.undefined;
+  });
+
+  it('should allow the user to test the datasource', done => {
+    datasource.testDatasource().then(response => {
+      expect(response).to.deep.equal({
+        status: 'success',
+        message: 'Your VividCortex datasource was successfully configured.',
+        title: 'Success',
+      });
+
+      done();
+    });
   });
 });
--- specs/lib/mocks.ts
+++ specs/lib/mocks.ts
@@ -1,7 +1,26 @@
-const backendSrv = {};
+const $q = require('q');
+const config = require('../../src/config.json');
+
+const backendSrv = {
+  requests: {
+    GET: {
+      metrics: {
+        status: 200,
+      },
+    },
+  },
+  datasourceRequest: options => {
+    const endpoint = options.url.replace(config.apiUrl, ''),
+      response = backendSrv.requests[options.method][endpoint];
+
+    if (!response) {
+      console.error('Backend request mock not found.');
+    }
+
+    return $q.when(response || {});
+  },
+};
 
 const templateSrv = {};
 
-const $q = {};
-
-export { backendSrv, templateSrv, $q };
+export { backendSrv, templateSrv };
