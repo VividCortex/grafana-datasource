03b986cfbc5797f542c8243eb82a7382948e92f9
Matias Chomicki <matias.chomicki@vividcortex.com>
2018-05-14 18:42:47 -0300

    0.0.1-beta build

---
+++ dist/config_ctrl.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="../node_modules/grafana-sdk-mocks/app/headers/common.d.ts" />
+export declare class VividCortexConfigCtrl {
+  static templateUrl: string;
+  current: any;
+  constructor();
+}
---
+++ dist/module.d.ts
@@ -0,0 +1,12 @@
+import VividCortexDatasource from './datasource';
+import { VividCortexQueryCtrl } from './query_ctrl';
+import { VividCortexConfigCtrl } from './config_ctrl';
+declare class VividCortexAnnotationsQueryCtrl {
+  static templateUrl: string;
+}
+export {
+  VividCortexDatasource as Datasource,
+  VividCortexQueryCtrl as QueryCtrl,
+  VividCortexConfigCtrl as ConfigCtrl,
+  VividCortexAnnotationsQueryCtrl as AnnotationsQueryCtrl,
+};
